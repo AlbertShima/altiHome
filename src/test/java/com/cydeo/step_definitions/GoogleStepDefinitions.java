86a0c45e6a019e51cd5fb721addefedb6c2d90a4
Albert Shima <albertshima@gmail.com>
2023-02-19 20:28:28 +0100

    Adding pages og googleSearch.feature

--- src/test/java/com/cydeo/step_definitions/GoogleStepDefinitions.java
+++ src/test/java/com/cydeo/step_definitions/GoogleStepDefinitions.java
@@ -14,6 +14,7 @@ public class GoogleStepDefinitions {
     public void  user_is_on_google_search_page() {
         Driver.getDriver().get("https://www.google.com");
     }
+
     @Then("user should see title is Google")
     public void user_should_see_title_is_google() {
         String expectedTitle = "Google";
@@ -21,10 +22,12 @@ public class GoogleStepDefinitions {
         Assert.assertEquals(actualTitle, expectedTitle);
         Driver.closeDriver();
     }
+
     @When("user types apple and clicks enter")
     public void user_types_apple_and_clicks_enter() {
         googleSearchPage.searchBox.sendKeys("apple" + Keys.ENTER);
     }
+
     @Then("user sees apple in the google title")
     public void user_sees_apple_in_the_google_title() {
         String expectedTitle = "apple - Google Search";
@@ -32,10 +35,12 @@ public class GoogleStepDefinitions {
         //Junit first element should be expected
         Assert.assertEquals("This is not the title", expectedTitle, actualTitle);
     }
+
     @When("user types {string} and clicks enter")
     public void userTypesAndClicksEnter(String searchKeyWord) {
         googleSearchPage.searchBox.sendKeys(searchKeyWord + Keys.ENTER);
     }
+
     @Then("user sees {string} in the google title")
     public void userSeesInTheGoogleTitle(String string) {
         String expectedTitle = string + " - Google Search";
